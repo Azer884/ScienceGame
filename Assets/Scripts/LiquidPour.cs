02183cf7c91795ff8cdf7d24b5a9fc61b16780be
Azer Hamdaoui <azer.hamdaoui.07@gmail.com>
2024-02-04 22:12:34 +0100

    FIX Bugs*

--- Assets/Scripts/LiquidPour.cs
+++ Assets/Scripts/LiquidPour.cs
@@ -8,13 +8,18 @@ public class LiquidPour : MonoBehaviour
 
     	private float fillSpeed = 0.01f;
         private LiquidVolume lv;
+        private Vector3 thePosition;
+
+        private float yMax;
     // Start is called before the first frame update
     void Start()
     {
         lv = transform.parent.GetComponent<LiquidVolume> ();
+        yMax = transform.parent.Find("TopLevel").localPosition.y;
     }
     private void Update() {
-        transform.position = new Vector3(0f, lv.liquidSurfaceYPosition - transform.localScale.y * 0.5f, 0f);
+        transform.localPosition = new Vector3(0f, WaterCheckPos(lv,yMax), 0f);
+        transform.localRotation = Quaternion.Euler (0, 0, 0);
     }
 
     void OnParticleCollision(GameObject other)
@@ -22,6 +27,14 @@ public class LiquidPour : MonoBehaviour
         if (lv.liquidLayers[0].amount < MaxWaterAmount(lv)) {
             lv.liquidLayers[0].amount += fillSpeed;
             lv.UpdateLayers(true);
+		}
+        if (lv.level >= 1f) 
+        {
+			transform.localRotation = Quaternion.Euler (Random.value * 30 - 15, Random.value * 30 - 15, Random.value * 30 - 15);
+		} 
+        else 
+        {
+			transform.localRotation = Quaternion.Euler (0, 0, 0);
 		}
     }
         
@@ -35,5 +48,13 @@ public class LiquidPour : MonoBehaviour
         }
 
         return maxWaterAmount;
+    }
+
+    float WaterCheckPos(LiquidVolume lv, float yMax)
+    {
+        float y;
+        y = yMax * (2 * lv.level -1) ;
+        
+        return y;
     }
 }
